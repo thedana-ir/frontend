f9a70e406cb894909a6c65bfb8cc9544bd369805
Najla Vahdani <najlavhdni@gmail.com>
2024-02-02 15:23:12 +0330

    additional codes were deleted

--- static/js/search.js
+++ static/js/search.js
@@ -6,19 +6,3 @@ Swal.fire({
     icon: 'error',
     confirmButtonText: 'Cool'
 })
-
-const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.onmouseenter = Swal.stopTimer;
-      toast.onmouseleave = Swal.resumeTimer;
-    }
-  });
-  Toast.fire({
-    icon: "success",
-    title: "Signed in successfully"
-  });
